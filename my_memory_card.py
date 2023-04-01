6ff378287d09a6115a32f30e2fd30b748ec8f312
shapova7ov <94638145+shap7valov@users.noreply.github.com>
2023-04-01 16:18:29 +0300

    Project
    Memory quiz

---
+++ my_memory_card.py
@@ -0,0 +1,162 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QButtonGroup, QRadioButton, QPushButton, QLabel)
+from random import shuffle
+ 
+app = QApplication([])
+btn_OK = QPushButton('Ответить') 
+lb_Question = QLabel('Самый сложный вопрос в мире!')
+ 
+RadioGroupBox = QGroupBox("Варианты ответов") 
+rbtn_1 = QRadioButton('Энцы')
+rbtn_2 = QRadioButton('Смурфы')
+rbtn_3 = QRadioButton('Чулымцы')
+rbtn_4 = QRadioButton('Алеуты')
+ 
+RadioGroup = QButtonGroup()
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout() 
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) 
+layout_ans3.addWidget(rbtn_4)
+ 
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3) 
+ 
+RadioGroupBox.setLayout(layout_ans1) 
+ 
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?')
+lb_Correct = QLabel('ответ будет тут!')
+ 
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+ 
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+ 
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+AnsGroupBox.hide() 
+ 
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) 
+layout_line3.addStretch(1)
+ 
+layout_card = QVBoxLayout()
+ 
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5)
+
+class Question():
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+
+q_list = []
+q_list.append(Question('Выбери русского пистаеля','Пушкин','Андерсен','Галилео','Эскобар'))
+q_list.append(Question('Выбери гения','тимоха','даник','тимохинсон','бака бак'))
+q_list.append(Question('выбери имя','гена','генадий','цидармян','аывлаоывла'))
+
+
+
+
+
+
+
+def show_result():
+    ''' показать панель ответов '''
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+    
+ 
+def show_question():
+    ''' показать панель вопросов '''
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False) 
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True) 
+ 
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+ 
+def ask(q: Question):
+    ''' функция записывает значения вопроса и ответов в соответствующие виджеты, 
+    при этом варианты ответов распределяются случайным образом'''
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_Question.setText(q.question)
+    lb_Correct.setText(q.right_answer) 
+    show_question() 
+ 
+def show_correct(res):
+    ''' показать результат - установим переданный текст в надпись "результат" и покажем нужную панель '''
+    lb_Result.setText(res)
+    show_result()
+ 
+def check_answer():
+    ''' если выбран какой-то вариант ответа, то надо проверить и показать панель ответов'''
+    if answers[0].isChecked():
+        show_correct('Правильно!')
+        window.score += 1
+    else:
+        if answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+            show_correct('Неверно!')
+
+def next_question():
+    shuffle(q_list)
+    ask(q_list[0])
+    q_list.remove(q_list[0])
+    window.total += 1
+    window.cur_question += 1
+    if window.cur_question >= len(q_list):
+        window.cur_question = 0
+
+def click_ok():
+    if btn_OK.text() == 'Ответить':
+        check_answer()
+        resultat()
+    else:
+        next_question()
+
+def resultat():
+    print('заданных:', window.total)
+    print('правильных:',window.score)
+    print('Рейтинг:',(window.score/window.total*100))
+
+window = QWidget()
+window.total = 0
+window.score = 0
+window.cur_question = 0
+window.setLayout(layout_card)
+window.setWindowTitle('Memo Card')
+next_question()
+btn_OK.clicked.connect(click_ok) 
+ 
+window.show()
+app.exec()
